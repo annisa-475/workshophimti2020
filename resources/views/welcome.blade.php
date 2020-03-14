23242e0d90595f1005c5d778361452d8f180afeb
pannisa-475 <pannisa475@gmail.com>
2020-03-14 11:53:06 +0700

    merubah tampilan welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    workshop
+                    annisa
                 </div>
 
                 <div class="links">
